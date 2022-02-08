2f372366d2d047db631c05be7ed2f6c771fba3d4
SirriCelles <sirricelles@gmail.com>
2022-02-08 05:18:52 +0100

    initial setup complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 import 'dotenv/config';
+
+const express = require('express');
+const app = express();
+const appPort = process.env.PORT;
+
+// setting up template engine
+app.set('view engine', 'ejs');
+
+// pointing to static files
+app.use(express.static(__dirname + '/public'));
+
+// get app routes
+app.get('/', function(req, res) {
+    res.render('pad');
+});
+
+
+console.log('');
+app.listen(appPort, () => {
+    console.log(`App Listening on Port ${appPort}!`);
+});
